4473fecd77a03e300db810da94675bb64edc0b68
Hossam Hamdy <hhamdy@abuerdan.com>
2024-04-19 06:28:38 +0200

    base url update

--- project-js/src/service/axios.js
+++ project-js/src/service/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
-
+let baseURL = "/";
+if (process.env.NODE_ENV === "development") {
+  baseURL = "http://127.0.0.1:8000/";
+}
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/",
+  // baseURL: "http://127.0.0.1:8000/",
+  baseURL: baseURL,
 });
 
 // Add a request interceptor to update the Authorization header before each request
